ab15bcc7a0e1a93e4aa546952430fcec49c8e2be
Priya-995 <priya9899nk@gmail.com>
2025-11-20 01:22:07 +0530

    authorization setup for listings

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Listing = require('./models/listing.js');
+const ExpressError=require('./utils/ExpressError.js')
+const {listingSchema,reviewSchema}=require('./schema.js')
+
+
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.redirectUrl=req.originalUrl
@@ -14,4 +19,32 @@ module.exports.saveRedirectUrl=(req,res,next)=>{
     }
     next();
 
+}
+module.exports.isOwner=async (req,res,next)=>{
+    let {id}=req.params
+  let listing=await Listing.findById(id);
+  if(!listing.owner._id.equals(res.locals.currUser._id)){
+    req.flash("failure","You don't have permissions");
+     return res.redirect(`/listings/${id}`)
+  }
+  next();
+
+}
+module.exports.validateListing=(req,res,next)=>{
+  let {error}=listingSchema.validate(req.body)
+  if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400,errMsg )
+  }else{
+    next();
+  }
+}
+module.exports.validateReview=(req,res,next)=>{
+  let {error}=reviewSchema.validate(req.body)
+  if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400,errMsg )
+  }else{
+    next();
+  }
 }
