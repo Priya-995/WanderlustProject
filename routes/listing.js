c05383a1d5b9d93a31e3a27c5d805fdcffd5feab
Priya-995 <priya9899nk@gmail.com>
2025-11-20 23:36:09 +0530

    implement MVC architecture and reorganize project structure

--- routes/listing.js
+++ routes/listing.js
@@ -1,97 +1,34 @@
 const express=require('express');
-const Listing = require('../models/listing.js');
 const wrapAsync=require('../utils/wrapAsync.js');
 
 const router = express.Router()
 const {isLoggedIn,isOwner,validateListing}=require('../middleware.js')
-
+const listingController=require("../controllers/listings.js")
 
 
 
 //⁡⁢⁢⁣index route⁡
-router.get("/",wrapAsync(async (req,res)=>{
-  //⁡⁣⁣⁢For printing the whole data⁡
-  // Listing.find({}).then((res)=>{
-  //   console.log(res)
-  // })
-  const {category}=req.query;
-  let filter={}
-  if(category&&category!='all'){
-    filter.category=category
-  }
-  
-  const listingsData= await Listing.find(filter)
-  res.render("listings/index.ejs",{listingsData,selectedCategory:category||'all'});
-
-}))
+router.get("/",wrapAsync(listingController.index))
 
 //NEW 
-router.get("/new",isLoggedIn,(req,res)=>{
-  res.render("listings/new.ejs")
-})
+router.get("/new",isLoggedIn,listingController.renderNewForm)
 
 //CREATE
 //⁡⁢⁢⁣A POST request is a way for a client (like your browser or app) to send data to a server.
 //Purpose: Usually to create new data or submit information.
 //Data goes in the body of the request (not the URL).⁡
-router.post("/",validateListing, wrapAsync(async(req, res, next) => {
-
-  
-  
-  const newListing = new Listing(req.body.listing);
-  newListing.owner=req.user._id;
-  await newListing.save();
-  req.flash("success","New listing Created!");
-  res.redirect("/listings");
-}));
+router.post("/",validateListing, wrapAsync(listingController.create));
 
 // SHOW ROUTE
-router.get("/:id",wrapAsync(async (req,res)=>{
-  let {id}=req.params
-  let listing=await Listing.findById(id).populate({path:"reviews",
-    populate:{
-      path:"author"
-    }
-
-  }).populate("owner")
-
-  if(!listing){
-    req.flash("failure","listing you requested for doesn't exist!")
-    return res.redirect("/listings")
-  }
-  res.render("listings/show.ejs",{listing})
-}))
+router.get("/:id",wrapAsync(listingController.show))
 
 //EDIT ROUTE
-router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(async (req,res)=>{
-  let {id}=req.params
-  const listing=await Listing.findById(id)
-  if(!listing){
-    req.flash("failure","listing you requested for doesn't exist!")
-    return res.redirect("/listings")
-  }
-  res.render("listings/edit.ejs",{listing})
-   
-}))
+router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm))
 
 // UPDATE ROUTE
-router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(async(req,res)=>{
-   if(!req.body||!req.body.listing){
-    throw new ExpressError(400, "Send valid data for listing");
-    
-  }
-  let {id}=req.params
-  await Listing.findByIdAndUpdate(id,{...req.body.listing})
-   req.flash("success","Listing updated!");
-  res.redirect(`/listings/${id}`)
-}))
+router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
 
 //DELETE ROUTE
-router.delete("/:id",isLoggedIn,isOwner,wrapAsync(async (req,res)=>{
-  let {id}=req.params
-  await Listing.findByIdAndDelete(id);
-    req.flash("success","Listing deleted!");
-  res.redirect("/listings")
+router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListing))
 
-}))
 module.exports = router
--- routes/review.js
+++ routes/review.js
@@ -1,36 +1,21 @@
 const express=require('express');
-const Listing = require('../models/listing.js');
+
 const wrapAsync=require('../utils/wrapAsync.js');
 const {isLoggedIn,validateReview,isReviewAuthor}=require('../middleware.js')
-const Review=require("../models/review.js")
+
 const router=express.Router({mergeParams:true});
+const ReviewController=require("../controllers/reviews.js")
 
 
 
 //REVIEW
 //POST ROUTE
-router.post("/",isLoggedIn,validateReview,wrapAsync(async(req,res)=>{
-  let listing=await Listing.findById(req.params.id);
-  let newReview=new Review(req.body.review) // associating author to review
-  newReview.author=req.user._id;
-  listing.reviews.push(newReview)
-  await newReview.save()
-  await listing.save()
-  req.flash("success","Thanks for sharing your thoughts!");
-  res.redirect(`/listings/${listing._id}`)
+router.post("/",isLoggedIn,validateReview,wrapAsync(ReviewController.createReview))
 
-}))
+
 //REVIEW
 //DELETE  ROUTE
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(async(req,res)=>{
-  let {id,reviewId}=req.params;
-  await Listing.findByIdAndUpdate(id,{$pull: {reviews:reviewId}})
-  // ⁡⁢⁢⁢The $pull operator removes from an existing array all instances of a value or values that match a specific condition.⁡
-  await Review.findByIdAndDelete(reviewId)
-  req.flash("success","Review removed successfully!");
-  res.redirect(`/listings/${id}`)
-
-}))
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(ReviewController.destroyReview))
 
 module.exports=router;
 
